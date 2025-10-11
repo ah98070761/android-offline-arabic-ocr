fb625dc537dd6be8bfbc3525fe84781d236d7efa
ah98070761 <ah98070761@gmail.com>
2025-10-11 16:14:36 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 // build.gradle.kts (ملف الجذر)
 
 plugins {
+    // تحديد الإصدارات الحديثة لـ AGP و Kotlin
     id("com.android.application") version "8.1.0" apply false 
     id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
