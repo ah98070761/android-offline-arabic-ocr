f9bbf92410e4ec98c1b985b68353b4b75ba3a62b
ah98070761 <ah98070761@gmail.com>
2025-10-11 16:05:15 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
 // build.gradle.kts (ملف الجذر)
 
 plugins {
-    // يجب تحديد الإصدار هنا
     id("com.android.application") version "8.1.0" apply false 
     id("com.android.library") version "8.1.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
-
-// لا تضع أي تهيئة لـ android { ... } أو dependencies { ... } هنا.
