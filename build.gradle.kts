d21521b980d364824c065c56307f558baf9f8f9a
ah98070761 <ah98070761@gmail.com>
2025-10-16 16:33:17 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,3 @@ plugins {
     id("com.android.library") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
-
-// ❌ إزالة كتلة allprojects: لم تعد ضرورية وتسبب خطأً.
