0518151d507124ebaebed6b39171e621b6aa7317
ah98070761 <ah98070761@gmail.com>
2025-10-17 21:21:25 +0300

    Update OcrResult.kt

--- app/src/main/java/com/example/ocr/data/OcrResult.kt
+++ app/src/main/java/com/example/ocr/data/OcrResult.kt
@@ -1,10 +1,14 @@
 package com.example.ocr.data
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 @Entity(tableName = "ocr_results")
 data class OcrResult(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
     val text: String
-)
+) : Parcelable
